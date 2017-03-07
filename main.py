354b154b488cb62e2141d74933038b7611ec3748
Marina-chan <marina.k.chan@gmail.com>
2017-03-07 23:29:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 import asyncio
+import glob
 import os
+import sqlite3
 from datetime import datetime
+from itertools import chain
 
 import aiohttp
 import imageio
@@ -12,37 +15,53 @@ import telepot.aio
 import config
 
 
+subs_db = sqlite3.connect("subscribers.db")
+subs_curr = subs_db.cursor()
 bot = telepot.aio.Bot(config.bot_token)
+loop = asyncio.get_event_loop()
 
 
 # rewrite for async/await ?
 def is_enough_for_gif():
-    files = [temp for temp in os.listdir('.') if os.path.isfile(temp) and temp.endswith(".png")]
-    if len(files) == 4:
-        imageio.mimsave("img.gif", list(map(imageio.read, files)), duration=1)
-        for file in files:
-            os.remove(file)
-    elif len(files) >= 4:
-        to_delete = min(files, key=os.path.getctime())
-        os.remove(to_delete)
-        is_enough_for_gif()
+    files = glob.glob("*.png")
+
+    flag = True
+    while flag:
+        if len(files) == 4:
+            imageio.mimsave("img.gif", list(map(imageio.imread, files)), duration=1)
+            for file in files[:-1]:
+                os.remove(file)
+                flag = False
+            loop.run_until_complete(send_to_subs())
+        elif len(files) >= 4:
+            to_delete = min(files, key=os.path.getctime)
+            os.remove(to_delete)
+            files.remove(to_delete)
+        elif len(files) < 4:
+            flag = False
 
 
 async def get_new_img():
     while True:
         async with aiohttp.ClientSession() as session:
             async with session.get(config.site) as resp:
-                with open(str(datetime.now().time()).replace(":", "-")[:8] + ".png", "wb") as file:
+                with open(datetime.now().strftime("%H-%M-%S") + ".png", "wb") as file:
                     file.write(await resp.read())
         is_enough_for_gif()
         await asyncio.sleep(60 * 15)
+
+async def send_to_subs():
+    subs_list = chain.from_iterable(subs_curr.execute("SELECT * FROM subscribers ORDER BY id"))
+    with open("img.gif", "rb") as gif_content:
+        for sub in subs_list:
+            await bot.sendDocument(sub, document=gif_content)
 
 
 async def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     command = msg['text']
-    print(str(datetime.now())[:19])
-    print("Chat message:", content_type, chat_type, chat_id)
+    print(str(datetime.now()))
+    print("Chat message:", content_type, chat_type, str(chat_id))
     print("Command:", command)
 
     if chat_type == "private" and content_type == "text":
@@ -52,15 +71,27 @@ async def handle(msg):
                 with open("img.gif", "rb") as gif_content:
                     await bot.sendDocument(chat_id, document=gif_content)
         elif command == "/current":
-            files = [temp for temp in os.listdir('.') if os.path.isfile(temp) and temp.endswith(".png")]
-            file = max(files, key=os.path.getctime())
+            files = glob.glob("*.png")
+            file = max(files, key=os.path.getctime)
             with open(file, "rb") as png_content:
                 await bot.sendPhoto(chat_id, photo=png_content)
+        elif command == "/sub":
+            if [] is subs_curr.execute("SELECT * FROM subscribers WHERE id=?", (chat_id, )).fetchall():
+                subs_curr.execute("INSERT INTO subscribers (id) VALUE (?)", (chat_id, ))
+                subs_db.commit()
+                await bot.sendMessage(chat_id, "You subscribed to bot")
+            else:
+                await bot.sendMessage(chat_id, "You already subscribed to bot")
+        elif command == "/unsub":
+            if [] is not subs_curr.execute("SELECT * FROM subscribers WHERE id=?", (chat_id, )).fetchall():
+                subs_curr.execute("DELETE FROM subscribers WHERE id=?", (chat_id, ))
+                subs_db.commit()
+                await bot.sendMessage(chat_id, "You unsubscribed from bot")
+            else:
+                await bot.sendMessage(chat_id, "You have not subscribed yet")
 
 
 def main():
-    loop = asyncio.get_event_loop()
-
     loop.create_task(bot.message_loop(handle))
     loop.create_task(get_new_img())
 
