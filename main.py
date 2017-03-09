0f46b926549a4d1b6c9181fc98146bb9dac0f32c
Marina-chan <marina.k.chan@gmail.com>
2017-03-10 01:23:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -18,19 +18,20 @@ import config
 
 subs_db = sqlite3.connect("subscribers.db")
 subs_curr = subs_db.cursor()
+subscribers = list(chain.from_iterable(subs_curr.execute("SELECT * FROM subscribers")))
 bot = telepot.aio.Bot(config.bot_token)
 loop = asyncio.get_event_loop()
 
 
 # rewrite for async/await ?
 def is_enough_for_gif():
-    files = glob.glob("*.png")
+    files = [file for file in glob.glob("*.png") if file != "current_weather.png"]
+    copyfile(files[-1], "current_weather.png")
 
     flag = True
     while flag:
         if len(files) == 4:
             imageio.mimsave("img.gif", list(map(imageio.imread, files)), duration=1)
-            copyfile(files[-1], "current_weather.png")
             for file in files:
                 os.remove(file)
                 flag = False
@@ -52,17 +53,17 @@ async def get_new_img():
         is_enough_for_gif()
         await asyncio.sleep(60 * 15)
 
+
 async def send_to_subs():
-    subs_list = chain.from_iterable(subs_curr.execute("SELECT * FROM subscribers ORDER BY id"))
     with open("img.gif", "rb") as gif_content:
-        for sub in subs_list:
+        for sub in subscribers:
             await bot.sendDocument(sub, document=gif_content)
 
 
 async def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     command = msg['text']
-    print(str(datetime.now()))
+    print(datetime.now().strftime("%d-%m-%Y %H:%M:%S"))
     print("Chat message:", content_type, chat_type, str(chat_id))
     print("Command:", command)
 
@@ -76,16 +77,18 @@ async def handle(msg):
             with open("current_weather.png", "rb") as png_content:
                 await bot.sendPhoto(chat_id, photo=png_content)
         elif command == "/sub":
-            if [] is subs_curr.execute("SELECT * FROM subscribers WHERE id=?", (chat_id, )).fetchall():
-                subs_curr.execute("INSERT INTO subscribers (id) VALUE (?)", (chat_id, ))
+            if chat_id not in subscribers:
+                subs_curr.execute("INSERT INTO subscribers (id) VALUES (?)", (chat_id, ))
                 subs_db.commit()
+                subscribers.append(chat_id)
                 await bot.sendMessage(chat_id, "You subscribed to bot")
             else:
                 await bot.sendMessage(chat_id, "You already subscribed to bot")
         elif command == "/unsub":
-            if [] is not subs_curr.execute("SELECT * FROM subscribers WHERE id=?", (chat_id, )).fetchall():
+            if chat_id in subscribers:
                 subs_curr.execute("DELETE FROM subscribers WHERE id=?", (chat_id, ))
                 subs_db.commit()
+                subscribers.remove(chat_id)
                 await bot.sendMessage(chat_id, "You unsubscribed from bot")
             else:
                 await bot.sendMessage(chat_id, "You have not subscribed yet")
